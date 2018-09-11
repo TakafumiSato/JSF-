ec6a8dfa87de2602b63bd8b199e3cd8478f00a03
佐藤孝史 <@DESKTOP-UU790TE>
2018-09-11 16:17:57 +0900

    Chapter7

---
+++ src/java/com/myjsf/AnswerBean.java
@@ -0,0 +1,58 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.myjsf;
+
+import javax.inject.Named;
+import javax.enterprise.context.SessionScoped;
+import java.io.Serializable;
+
+/**
+ *
+ * @author 佐藤孝史
+ */
+@Named(value = "answerBean")
+@SessionScoped
+public class AnswerBean implements Serializable {
+
+    String gender;
+    String brand;
+    String brandName;
+    final String[] brandList = {
+        "MarcJacobs","EMODA","EGOIST","MURUA","dazzlin","その他"
+    };
+    
+    public AnswerBean() {
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+        setBrandName(brandList[Integer.parseInt(brand)-1]);
+    }
+
+    public String getBrandName() {
+        return brandName;
+    }
+
+    public void setBrandName(String brandName) {
+        this.brandName = brandName;
+    }
+    
+    public String[] getBrandList() {
+        return brandList;
+    }
+}
