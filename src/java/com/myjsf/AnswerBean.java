f7dd54a02270bb84ea49c79ea3a142c45731a840
TakafumiSato <sato@augusta-inc.co.jp>
2018-09-12 09:48:21 +0900

    Chapter7 完成

--- src/java/com/myjsf/AnswerBean.java
+++ src/java/com/myjsf/AnswerBean.java
@@ -16,10 +16,43 @@ import javax.enterprise.context.RequestScoped;
 @RequestScoped
 public class AnswerBean {
 
-    /**
-     * Creates a new instance of AnswerBean
-     */
+    String gender;
+    String brand;
+    String brandName;
+    final String[] brandList = {
+        "MarcJacobs","EMODA","EGOIST","MURUA","dazzlin","その他"
+    };
+    
     public AnswerBean() {
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+        setBrandName(brandList[Integer.parseInt(brand)-1]);
+    }
+
+    public String getBrandName() {
+        return brandName;
+    }
+
+    public void setBrandName(String brandName) {
+        this.brandName = brandName;
+    }
+    
+    public String[] getBrandList() {
+        return brandList;
     }
     
 }
